6e8b76544f9063c67e8dc40f3233289a32edbe29
kieykouch <ki.ey.kouch@gmail.com>
2016-03-05 18:30:34 -0800

    get shaking to work

--- App/WeVotethis/wear/src/main/java/com/example/kieykouch/wevotethis/MainActivity.java
+++ App/WeVotethis/wear/src/main/java/com/example/kieykouch/wevotethis/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.kieykouch.wevotethis;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
     private String catName = "";
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +29,51 @@ public class MainActivity extends Activity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
-
         if (extras != null) {
             String catName = extras.getString("CAT_NAME");
         }
+
+
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        mShakeDetector = new ShakeDetector();
+//        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+//            @Override
+//            public void onShake() {
+//                mTextView.setText("I'm Shaking this");
+//            }
+//        });
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                mTextView.setText("I'm Shaking this");
+
+                Random rand = new Random();
+                int x = rand.nextInt(99999);
+                
+
+            }
+        });
+
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
